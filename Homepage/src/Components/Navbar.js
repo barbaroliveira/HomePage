d2a736dc9a13c3b277f266dfe6aa4fbf350c0a6c
Bárbara Pereira Poças Oliveira <89589498+barbaroliveira@users.noreply.github.com>
2023-06-16 18:51:08 +0100

    responsive navbar

--- Homepage/src/Components/Navbar.js
+++ Homepage/src/Components/Navbar.js
@@ -1,23 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unreachable */
 /* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import Logo from "../Assets/LOGO.svg";
-import {BsCart2} from "react-icons/bs";
+import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
-/* import {
-     Box,
-     Drawer,
-     ListItem,
-     ListItemButton,
-     ListItemIcon,
-     ListItemText
-    } from "@mui/material"; */
-    import HomeIcon from "@mui/icons-material/Home";
-    /* import InfoIcon from "@mui/icons-material/Info";
-    import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-    import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-    import { ShoppingCartRoundedIcon } from "@mui/icons-material/ShoppingCartRounded"; */    
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 const Navbar = () => {
 
@@ -29,7 +30,7 @@ const Navbar = () => {
         },
         {
             text:"Work",
-            icon:<HomeIcon/>
+            icon:<InfoIcon/>
         }
     ]
 
@@ -47,10 +48,28 @@ const Navbar = () => {
             <button className="primary-button">
                 Bookings Now
             </button>
+        </div>
             <div className="navbar-menu-container">
                 <HiOutlineBars3 onClick={() => setOpenMenu(true)}/>
             </div>
-        </div>
+        <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+            <Box sx={{ width: 250 }}
+            role="presentation"
+            onClick= {() => setOpenMenu(false)}
+            onKeyDown= {() =>setOpenMenu(false)}
+            >
+                <List>
+                    {menuOptions.map((item) => (
+                        <ListItem key={item.text} disablePadding >
+                            <ListItemButton>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+        </Drawer>
     </nav>
     );
 }
